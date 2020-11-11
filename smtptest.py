c03600fcebd6eedfdd11e7e0be757dd92973e3d1
Ivan Buetler <ivan.buetler@compass-security.com>
2020-11-11 10:04:04 +0100

    fixing error: ValueError: server_hostname cannot be an empty string or start with a leading dot.

--- smtptest.py
+++ smtptest.py
@@ -78,7 +78,7 @@ server = None
 if options.usessl:
 	server = smtplib.SMTP_SSL()
 else:
-	server = smtplib.SMTP()
+	server = smtplib.SMTP(serveraddr)
 
 server.set_debuglevel(options.debuglevel)
 server.connect(serveraddr, options.serverport)
